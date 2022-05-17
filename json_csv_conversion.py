8e1143ed6151747b597a4ceee9ef9f1390230e6a
pierre-jezegou <pierre.jezegou.etu@univ-lille.fr>
2022-05-17 16:22:24 +0200

    [Amélioration] Ajout try/except

--- json_csv_conversion.py
+++ json_csv_conversion.py
@@ -1,15 +1,16 @@
 import json
 import os
 from sympy import classify_pde
+from tqdm import tqdm
 
 PATH="./"
 SEP = ", "
 
 
-fichiers = ["patients", "conditions", "allergies", "visites", "prescriptions", "obs_num", "obs_text"]
+fichiers = ["patients", "conditions", "allergies", "visites", "prescriptions", "obs_num", "obs_text", "ref_medications"]
 
 
-def INITIALISATION()->None:
+def INITIALISATION() -> None:
     print("Nettoyage de l'espace de travail...")
     for fichier in fichiers:
         if os.path.exists(fichier+".csv"):
@@ -20,26 +21,39 @@ def INITIALISATION()->None:
 
 
 def JSON_CSV_CONVERTER(json_path:str) -> None:
+    assert(os.path.exists(json_path))
     print("Début de la conversion")
-    with open("Donnes_demo_pharmIA.json", "r") as fichier:
+    with open(json_path, "r") as fichier:
         all_data = json.load(fichier)
-        print("\tfichier ouvert")
+        print("\tFichier ouvert")
     # taille_json = len(all_data["entry"])
     for evenement in all_data["entry"]:
         evenement = evenement["resource"]
-        if evenement["resourceType"]=="Patient":
-            add_patient(evenement)
-        elif evenement["resourceType"]=="Condition":
-            add_condition(evenement)
-        elif evenement["resourceType"]=="AllergyIntolerance":
-            add_allergy(evenement)
-        elif evenement["resourceType"]=="Encounter":
-            add_encounter(evenement)
-        elif evenement["resourceType"]=="MedicationRequest":
-            add_prescription(evenement)
-        elif evenement["resourceType"]=="Observation":
-            add_observation(evenement)
-            
+        try:
+            if evenement["resourceType"]=="Patient":
+                __add_patient(evenement)
+            elif evenement["resourceType"]=="Condition":
+                __add_condition(evenement)
+            elif evenement["resourceType"]=="AllergyIntolerance":
+                __add_allergy(evenement)
+            elif evenement["resourceType"]=="Encounter":
+                __add_encounter(evenement)
+            elif evenement["resourceType"]=="MedicationRequest":
+                __add_prescription(evenement)
+            elif evenement["resourceType"]=="Medication":
+                __add_medication(evenement)
+            elif evenement["resourceType"]=="Observation":
+                __add_observation(evenement)
+            elif evenement["resourceType"]=="Location":
+                i = 1
+            elif evenement["resourceType"]=="Organization":
+                i = 1
+            else:
+                print("Cet évènement ne correspond à aucun type connu.")
+                print(evenement)
+        except KeyError:
+            print("error")
+    print("\tTraitement terminé")
     return None
 
 def ouverture_fichier(filename:str, path:str =PATH, mode:str="a"):
@@ -48,7 +62,7 @@ def ouverture_fichier(filename:str, path:str =PATH, mode:str="a"):
 
 
 
-def add_patient(evenement:dict, IA_mode=False)->None:
+def __add_patient(evenement:dict, IA_mode=False)->None:
     ''' Ajout d'un patient au fichier 'patients.csv' '''
     fichier = ouverture_fichier("patients.csv")
     id = evenement["identifier"][0]["value"]
@@ -65,7 +79,7 @@ def add_patient(evenement:dict, IA_mode=False)->None:
 
 
 
-def add_condition(evenement:dict, IA_mode=False)->None:
+def __add_condition(evenement:dict, IA_mode=False)->None:
     ''' Ajout d'une condition au fichier 'conditions.csv' '''
     fichier = ouverture_fichier("conditions.csv")
     id = evenement["id"][1:]
@@ -83,7 +97,7 @@ def add_condition(evenement:dict, IA_mode=False)->None:
 
 
 
-def add_allergy(evenement:dict, IA_mode=False)->None:
+def __add_allergy(evenement:dict, IA_mode=False)->None:
     ''' Ajout d'une allergie au fichier 'allergies.csv' '''
     fichier = ouverture_fichier("allergies.csv")
     id = evenement["id"][1:]
@@ -102,7 +116,7 @@ def add_allergy(evenement:dict, IA_mode=False)->None:
 
 
 
-def add_encounter(evenement:dict, IA_mode=False)->None:
+def __add_encounter(evenement:dict, IA_mode=False)->None:
     ''' Ajout d'une allergie au fichier 'visites.csv' '''
     fichier = ouverture_fichier("visites.csv")
     id = evenement["id"][1:]
@@ -123,7 +137,7 @@ def add_encounter(evenement:dict, IA_mode=False)->None:
 
 
 
-def add_prescription(evenement:dict, IA_mode=False) -> None:
+def __add_prescription(evenement:dict, IA_mode=False) -> None:
     ''' Ajout d'une allergie au fichier 'prescriptions.csv' '''
     fichier = ouverture_fichier("prescriptions.csv")
     id = evenement["id"][1:]
@@ -136,7 +150,7 @@ def add_prescription(evenement:dict, IA_mode=False) -> None:
     frequence = evenement["dosageInstruction"][0]["text"]
     si_besoin = evenement["dosageInstruction"][0]["asNeededCodeableConcept"]["text"]
     commentaire = ""
-    status = evenement["status"]
+    status = "A valider"
     ip = ""
     appel = ""
 
@@ -146,7 +160,7 @@ def add_prescription(evenement:dict, IA_mode=False) -> None:
     return None
 
 
-def add_observation(evenement:dict, IA_mode=False) -> None:
+def __add_observation(evenement:dict, IA_mode=False) -> None:
     ''' Ajout d'une allergie au fichier 'obs_num.csv' '''
     
     observation_type = evenement["code"]["coding"][0]["code"]
@@ -173,3 +187,12 @@ def add_observation(evenement:dict, IA_mode=False) -> None:
         fichier.write(line)
     fichier.close()
     return None
+
+
+def __add_medication(evenement:dict, IA_mode=False):
+    ''' Ajout d'un médicament au fichier 'ref_medications.csv' '''
+    fichier = ouverture_fichier("ref_medications.csv")
+    code = evenement["code"]["coding"][0]["code"]
+    display = evenement["code"]["coding"][0]["display"].replace(',', '.')
+    fichier.write(code +SEP+ display +"\r")
+    fichier.close()
