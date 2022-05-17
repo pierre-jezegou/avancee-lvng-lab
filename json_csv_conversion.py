823daf1362511eac6f641d829da23e71b01460d3
pierre-jezegou <pierre.jezegou.etu@univ-lille.fr>
2022-05-17 10:54:19 +0200

    Ajout de la fonction d'ajout des allergies

--- json_csv_conversion.py
+++ json_csv_conversion.py
@@ -1,26 +1,29 @@
 import json
+import os
+from sympy import classify_pde
 
 PATH="./"
 SEP = ", "
 
-# # Ouverture du fichier et parsing des données json->dictionnaire python
-# with open("Donnes_demo_pharmIA.json", "r") as fichier:
-#     all_data = json.load(fichier)
 
-# print(len(all_data["entry"]))
-# for i in range(10):
-#     # print(all_data["entry"][i])
-#     print(type(all_data["entry"][i]["resource"]))
-#     print(all_data["entry"][i]["resource"]["essai"])
-#     # data = json.loads(all_data["entry"][i])
-#     # print(data)
-# # for data in all_data:
-# #     data = json.dumps(data)
-# #     print(data)
+fichiers = ["patients", "conditions", "allergies", "visites", "prescriptions", "obs_num", "obs_text"]
+
+
+def INITIALISATION()->None:
+    print("Nettoyage de l'espace de travail...")
+    for fichier in fichiers:
+        if os.path.exists(fichier+".csv"):
+            os.remove(fichier+".csv")
+    print("\tFichiers supprimés\n")
+
+
+
 
 def JSON_CSV_CONVERTER(json_path:str) -> None:
+    print("Début de la conversion")
     with open("Donnes_demo_pharmIA.json", "r") as fichier:
         all_data = json.load(fichier)
+        print("\tfichier ouvert")
     # taille_json = len(all_data["entry"])
     for evenement in all_data["entry"]:
         evenement = evenement["resource"]
@@ -28,6 +31,8 @@ def JSON_CSV_CONVERTER(json_path:str) -> None:
             add_patient(evenement)
         elif evenement["resourceType"]=="Condition":
             add_condition(evenement)
+        elif evenement["resourceType"]=="AllergyIntolerance":
+            add_allergy(evenement)
     return None
 
 def ouverture_fichier(filename:str, path:str =PATH, mode:str="a"):
@@ -54,7 +59,7 @@ def add_patient(evenement:dict, IA_mode=False)->None:
 
 
 def add_condition(evenement:dict, IA_mode=False)->None:
-    ''' Ajout d'un patient au fichier 'conditions.csv' '''
+    ''' Ajout d'une condition au fichier 'conditions.csv' '''
     fichier = ouverture_fichier("conditions.csv")
     id = evenement["id"][1:]
     condition = evenement["code"]["coding"][0]
@@ -70,20 +75,25 @@ def add_condition(evenement:dict, IA_mode=False)->None:
 
 
 
-def add_condition(evenement:dict, IA_mode=False)->None:
-    ''' Ajout d'un patient au fichier 'conditions.csv' '''
-    fichier = ouverture_fichier("conditions.csv")
+
+def add_allergy(evenement:dict, IA_mode=False)->None:
+    ''' Ajout d'une allergie au fichier 'allergies.csv' '''
+    fichier = ouverture_fichier("allergies.csv")
     id = evenement["id"][1:]
-    condition = evenement["code"]["coding"][0]
-    system = condition["system"]
-    code = condition["code"]
-    display = condition["display"]
-    patient = evenement["subject"]["reference"].replace("Patient/i", '')
+    allergy_type = evenement["type"]
+    category = evenement["category"][0]
+    description = evenement["code"]["coding"][0]
+    code = description["code"]
+    display = description["display"]
+    ipp = evenement["patient"]["reference"].replace("Patient/i", '')
+    iep = evenement["encounter"]["reference"].replace("Encounter/i", '')
 
     if not(IA_mode):
-        fichier.write(id +SEP+ patient +SEP+ system +SEP+ code +SEP+ display+"\r")
+        fichier.write(id +SEP+ ipp +SEP+ iep +SEP+ code +SEP+ display+"\r")
     fichier.close()
     return None
 
-    
-JSON_CSV_CONVERTER("Donnes_demo_pharmIA.json")
+
+
+
+
